12901711cc76ea8c9a2ead3a5b06bac601384881
Denis <d-kasakow@mail.de>
2023-02-12 13:26:06 +0100

    lösung ohne structs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include ""
+#include "main.h"
 
 int main()
 {
@@ -13,13 +13,13 @@ int main()
 	ui.setIm(5);
 	ui.printZahl(0);
 
-	ui.setPolar(5, 45);
+	ui.setPolar(5, 0.8);
 	ui.printZahl(0);
 
 	ui.setBetrag(3);
 	ui.printZahl(0);
 
-	ui.setWinkel(90);
+	ui.setWinkel(1.6);
 	ui.printZahl(0);
 
 	return 0;
